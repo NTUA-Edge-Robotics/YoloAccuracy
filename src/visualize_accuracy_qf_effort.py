c309a3dbd2db0fc4a3f282ee4033546939656bd8
Olivier Brochu <olivier_brochu@hotmail.com>
2022-07-14 12:20:46 +0300

    Added way to visualize accuracy according to quality factor and effort

--- src/visualize_accuracy_qf_effort.py
+++ src/visualize_accuracy_qf_effort.py
@@ -1,11 +1,12 @@
 import argparse
 import pandas
-from matplotlib import pyplot
+from matplotlib import pyplot, ticker
 from mpl_toolkits import mplot3d
 
 from compute_accuracy_as_binary_classification import compute_accuracy_as_binary_classification
+from convert_effort_to_integer import convert_effort_to_integer
 
-parser = argparse.ArgumentParser(description="Visualize the average accuracy of YOLO predictions according to height and quality factor.")
+parser = argparse.ArgumentParser(description="Visualize the average accuracy of YOLO predictions according to the quality factor and the effort.")
 
 parser.add_argument("json", help="Path to the JSON results")
 parser.add_argument("figure", help="Path and filename of the resulting figure")
@@ -17,20 +18,25 @@ frame = pandas.read_json(args.json)
 # Convert predicted and actual classes to a binary classification
 compute_accuracy_as_binary_classification(frame)
 
-frame = frame.groupby(["quality", "height"], as_index = False)["accuracy"].mean()
+# Convert effort to integer
+unique_efforts = convert_effort_to_integer(frame)
+
+# Group by quality and effort
+frame = frame.groupby(["quality", "effort_id"], as_index = False)["accuracy"].mean()
 
 # Prepare graph
 pyplot.figure(figsize=(3.4, 4))
 axes = pyplot.axes(projection="3d")
 
-axes.bar(frame["quality"], frame["accuracy"], zs=frame["height"], zdir="y", width = 5)
+axes.bar(frame["quality"], frame["accuracy"], zs=frame["effort_id"], zdir="y", width = 5)
 
 # Labelling
 axes.set_xlabel("Quality")
 pyplot.xticks(frame["quality"].unique(), rotation=-40)
 
-axes.set_ylabel("Height (pixels)")
-pyplot.yticks(frame["height"].unique(), rotation=50)
+axes.set_ylabel("Effort")
+axes.set_yticks(frame["effort_id"].unique())
+axes.set_yticklabels(unique_efforts, rotation=20)
 
 axes.set_zlabel("Average Accuracy")
 
---
+++ src/visualize_accuracy_qf_height.py
@@ -0,0 +1,41 @@
+import argparse
+import pandas
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
+
+from compute_accuracy_as_binary_classification import compute_accuracy_as_binary_classification
+
+parser = argparse.ArgumentParser(description="Visualize the average accuracy of YOLO predictions according to the quality factor and the height.")
+
+parser.add_argument("json", help="Path to the JSON results")
+parser.add_argument("figure", help="Path and filename of the resulting figure")
+
+args = parser.parse_args()
+
+frame = pandas.read_json(args.json)
+
+# Convert predicted and actual classes to a binary classification
+compute_accuracy_as_binary_classification(frame)
+
+# Group by quality and height
+frame = frame.groupby(["quality", "height"], as_index = False)["accuracy"].mean()
+
+# Prepare graph
+pyplot.figure(figsize=(3.4, 4))
+axes = pyplot.axes(projection="3d")
+
+axes.bar(frame["quality"], frame["accuracy"], zs=frame["height"], zdir="y", width = 5)
+
+# Labelling
+axes.set_xlabel("Quality")
+pyplot.xticks(frame["quality"].unique(), rotation=-40)
+
+axes.set_ylabel("Height (pixels)")
+pyplot.yticks(frame["height"].unique(), rotation=50)
+
+axes.set_zlabel("Average Accuracy")
+
+axes.view_init(23, -139)
+
+pyplot.tick_params(axis="both", which="both", labelsize=6)
+pyplot.savefig(args.figure, bbox_inches="tight")
